969711ff8d1ef8fd2f2522da72562620af3948d4
AryanGupta9803 <aryan@u.nus.edu>
2023-06-06 10:53:17 +0530

    Firebase auth for login

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import firebase_admin
 from firebase_admin import credentials, auth
 
 app = Flask(__name__)
+
+cred = credentials.Certificate('zaisei-kuhaku-firebase-adminsdk-534l6-98bbc60897.json')
+firebase_admin.initialize_app(cred)
+
+
