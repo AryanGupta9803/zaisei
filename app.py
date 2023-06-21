41bc0025bc1183dec875022368a5a0912aca9906
AryanGupta9803 <aryan@u.nus.edu>
2023-06-21 23:44:27 +0530

    databse key

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from firebase_admin import credentials, auth
 
 app = Flask(__name__)
 
-cred = credentials.Certificate('zaisei-kuhaku-firebase-adminsdk-534l6-98bbc60897.json')
+cred = credentials.Certificate('kuhaku-admin.json')
 firebase_admin.initialize_app(cred)
 
